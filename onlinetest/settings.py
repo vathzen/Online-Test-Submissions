d09331a62374e0fd5787621ab7b61f7d8fa18737
Vathsan <srivaths311@gmail.com>
2020-06-02 18:46:00 +0530

    Random Chikibum

--- onlinetest/settings.py
+++ onlinetest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '72*hfol%qqj@)7dblzvu2n!oafya3%_%!8z%%bgk5%a$14#hg1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vathsantest.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['vathsantest.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
