3efbf625f01f57becc0e776b81125f7808881cdf
Vathsan <srivaths311@gmail.com>
2020-06-02 19:11:10 +0530

    UI Update

--- home/forms.py
+++ home/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    name = forms.CharField(label='Name',max_length=20)
-    uploadedFile = forms.FileField(label='Answer Script:')
+    name = forms.CharField(max_length=20)
+    uploadedFile = forms.FileField()
